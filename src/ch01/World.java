1ea54b398da9ffb0383ee5589c11051f95f10a4d
butterfly <butterfly@naver.com>
2024-01-23 11:52:43 +0900

    두 개의 파일이 수정되었습니다.

--- src/ch01/World.java
+++ src/ch01/World.java
@@ -1,8 +1,8 @@
 package ch01;
 
 public class World {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         System.out.println("world01");
+        System.out.println("world02");
     }
 }
